cafae0d10cd9da7ffd22174d758e4a6a53216157
Smartin8411 <martinonyimba@gmail.com>
2021-01-04 16:37:24 -0500

    removed clock function in script.js

--- script.js
+++ script.js
@@ -17,33 +17,8 @@ function toggleNavList(){
     navbarList.classList.toggle('navbar-closed');
 }
 
-//JS Home Page Clock
-setInterval(currentTime, 1000)//represents milliseconds for each second
-
-const secondHand = document.querySelector('.second-hand')
-const minuteHand = document.querySelector('.minute-hand')
-const hourHand = document.querySelector('.hour-hand')
-
-function currentTime() {
-  const time = new Date()
-  const seconds = time.getSeconds() / 60
-  const minutes = (seconds + time.getMinutes()) / 60
-  const hours = (minutes + time.getHours()) / 12
-  setupRotation(secondHand, seconds);
-  setupRotation(minuteHand, minutes);
-  setupRotation(hourHand, hours);
-}
-
-function setupRotation(element, rotateRatio) {
-  element.style.setProperty('--rotation', rotateRatio * 360);
-}
-
-currentTime();
-
 //Updated Full Year Events/Current Date
-
 let currentYear = document.querySelector('#current-year');
-
 let thisDay = new Date();
 
 currentYear.innerText = thisDay.getFullYear();
